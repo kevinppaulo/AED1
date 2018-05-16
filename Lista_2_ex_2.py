1ddbd1a881ec4628557a03d2a00ccc3a923b6dcb
Kevin Paulo <kevinppaulo@gmail.com>
2018-05-16 10:25:29 -0300

    second commit

---
+++ Lista_2_ex_2.py
@@ -0,0 +1,17 @@
+lista = []
+positivos = []
+negativos = []
+for i in range(0,10):
+    try:
+        lista.append(int(input("Digite um numero: ")))
+    except:
+        print("você não deve digitar letras ou simbolos")
+
+for item in range(0,10):
+    if lista[item] >= 0:
+        positivos.append(lista[item])
+    else:
+        negativos.append(lista[item])
+print(lista)
+print(positivos)
+print(negativos)
